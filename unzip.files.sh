40bfe2ce5165b2320e247b9a2d37526b961077df
zhongguojie1998 <zhongguojie1998@gmail.com>
2024-03-20 15:55:03 -0400

    Add large .tgz files tracked with Git LFS

---
+++ unzip.files.sh
@@ -0,0 +1,15 @@
+# unzip files in the data.files/ folder
+cd data.files/
+cat esm.files.tgz.part-* | tar -xzvf -
+cat esm.MSA.tgz.part-* | tar -xzvf -
+for tgzfile in `ls *.tgz`; do echo "Unzipping $tgzfile"; tar -xzvf $tgzfile; done
+cd ../
+# unzip files in the PreMode.results/ folder
+cd PreMode.results/
+cat PreMode.mean.var.seed.0.tgz.part-* | tar -xzvf -
+for tgzfile in `ls *.tgz`; do echo "Unzipping $tgzfile"; tar -xzvf $tgzfile; done
+cd ../
+# unzip files in the analysis/ folder
+cd analysis/
+for tgzfile in `ls *.tgz`; do echo "Unzipping $tgzfile"; tar -xzvf $tgzfile; done
+cd ../
