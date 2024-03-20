6db2ae2054c2a51d0993785f8d1c02a7cdb2f8de
zhongguojie1998 <zhongguojie1998@gmail.com>
2024-03-20 17:45:32 -0400

    Uploaded data.files/

--- unzip.files.sh
+++ unzip.files.sh
@@ -2,7 +2,9 @@
 cd data.files/
 cat esm.files.tgz.part-* | tar -xzvf -
 cat esm.MSA.tgz.part-* | tar -xzvf -
+cat gMVP.MSA.tgz.part-* | tar -xzvf -
 for tgzfile in `ls *.tgz`; do echo "Unzipping $tgzfile"; tar -xzvf $tgzfile; done
+for gzfile in `ls pretrain/*.gz`; do echo "Unzipping $gzfile"; gunzip $gzfile; done
 cd ../
 # unzip files in the PreMode.results/ folder
 cd PreMode.results/
