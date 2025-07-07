bdf3f0c3c3691172224e88dea6362afb7a59cd2d
zhongguojie1998 <zhongguojie1998@gmail.com>
2025-07-06 20:03:54 -0400

    Remove unused scripts

--- infer.all.in.4.cards.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-hs=$(hostname)
-echo $hs
-nohup bash scripts/DMS.5fold.inference.sh $1 $2 P15056.itan.split.large.window,P07949.itan.split.large.window 0 > $hs.log.0.txt &
-nohup bash scripts/DMS.5fold.inference.sh $1 $2 P04637.itan.split.large.window,Q09428.itan.split.large.window 1 > $hs.log.1.txt &
-nohup bash scripts/DMS.5fold.inference.sh $1 $2 O00555.itan.split.large.window,Q14524.clean.itan.split.large.window 2 > $hs.log.2.txt &
-nohup bash scripts/DMS.5fold.inference.sh $1 $2 Q99250.itan.split.large.window,Q14654.itan.split.large.window,P21802.itan.split.large.window 3 > $hs.log.3.txt &
--- transfer.all.in.one.card.sh
+++
@@ -1,2 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-nohup bash scripts/DMS.5fold.run.sh $1 P15056,P21802,P07949,Q09428,O00555,Q14654,Q99250,Q14524,P04637 0 > euler.log.0.txt &
